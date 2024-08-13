c92f3d483ebac0f2cfd86531cb3ae899f761b13d
Chris Roberts <chrisrobertscollective@gmail.com>
2024-08-13 12:31:51 +0100

    adding transition

---
+++ components/CarDetails.tsx
@@ -0,0 +1,19 @@
+import { CarProps } from '@/types';
+import React from 'react'
+import Image from 'next/image'
+import { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+interface CarDetailsProps {
+  isOpen: boolean;
+  closeModal : () => void;
+  car: CarProps
+}
+
+const CarDetails = ({isOpen,closeModal, car }: CarDetailsProps) => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default CarDetails
