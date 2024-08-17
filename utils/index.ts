9fd7c6df6cc2673f2f28688cc1825b132c562477
Chris Roberts <chrisrobertscollective@gmail.com>
2024-08-17 20:23:34 +0100

    issue with api request

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,21 @@
 import { CarProps } from "@/types";
 import { FilterProps } from "@/types";
 
-export async function fetchCars(filters: FilterProps) {
+export async function fetchCars() {
   // const { manufacturer, year, model, limit, fuel } = filters;
+
+  const apiKey = process.env.RAPID_API_KEY;
+  if (!apiKey) {
+    throw new Error("API key not found");
+  }
 
   // Set the required headers for the API request
   const headers: HeadersInit = {
-    "X-RapidAPI-Key": process.env.RAPID_API || "",
+    "X-RapidAPI-Key": apiKey,
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', { headers });
+  const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla `, { headers });
 
   const result = await response.json();
   console.log(result);
