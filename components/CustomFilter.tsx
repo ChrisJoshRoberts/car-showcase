275c99eb29774d8126ec66118d26cd543231fd86
Chris Roberts <chrisrobertscollective@gmail.com>
2024-08-30 11:02:48 +0100

    adding fuctionality to my custom filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -3,13 +3,26 @@ import React, { useState, Fragment } from 'react'
 import Image from 'next/image'
 import { Listbox, Transition } from '@headlessui/react'
 import { CustomFilterProps } from '@/types'
+import { useRouter } from 'next/router'
+import { updateSearchParams } from '@/utils'
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0])
+  const router = useRouter()
 
+  const handleUpdateParams = (e: {title: string, value: string}) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase())
+    router.push(newPathName)
+  }
   return (
     <div className="w-fit">
-      <Listbox value={selected} onChange={setSelected}>
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams(e);
+        }}
+        >
         <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
             <span className='block truncate'>{selected.title}</span>
