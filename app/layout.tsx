6b2639c8a79712195dee2d81cb83213cbc83bc59
Chris Roberts <chrisrobertscollective@gmail.com>
2024-09-05 14:23:57 +0200

    adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/public/favicon.ico" />
+      </head>
       <body className="relative">
         <NavBar />
         {children}
